ebb421b8281c10fd4298be9070f7eb22cbf05ea4
nirmalks736 <151901976+nirmalks736@users.noreply.github.com>
2023-12-12 10:14:35 +0000

    Update 9large&small.c

--- Sem1/ADS/9large&small.c
+++ Sem1/ADS/9large&small.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-
 void main(){
      int lar,sam,a[10],i,size;
      printf("Enter size of array: ");
@@ -8,17 +7,7 @@ void main(){
      for(i=0;i<size;i++){
           printf("\nArray[%d] ",i+1);
           scanf("%d",&a[i]);
-     }
-     
-     /*for(i=0;i<size;i++){
-      if(a[i]>a[i-1]){
-        lar=a[i];
-      }
-      if(a[i]<a[i-1]){
-        sam=a[i];
-      }
-     }*/
-     
+     }    
      lar=sam=a[0];
      for(i=0;i<size;i++){
           if(lar>a[i]){
