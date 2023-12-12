a2830b62ddc1e66835997f1aea1f5f1a6567853a
nirmalks736 <151901976+nirmalks736@users.noreply.github.com>
2023-12-12 10:18:32 +0000

    Update 11queue.c

--- Sem1/ADS/11queue.c
+++ Sem1/ADS/11queue.c
@@ -4,57 +4,57 @@ struct Queue {
     int front, rear;
     int arr[MAX_SIZE];
 };
-void initializeQueue(struct Queue *queue) {
-    queue->front = -1;
-    queue->rear = -1;
+void initializeQueue(struct Queue * queue) {
+    queue -> front = -1;
+    queue -> rear = -1;
 }
-int isEmpty(struct Queue *queue) {
-    return (queue->front == -1 && queue->rear == -1);
+int isEmpty(struct Queue * queue) {
+    return (queue -> front == -1 && queue -> rear == -1);
 }
-int isFull(struct Queue *queue) {
-    return (queue->rear == MAX_SIZE - 1);
+int isFull(struct Queue * queue) {
+    return (queue -> rear == MAX_SIZE - 1);
 }
-void enqueue(struct Queue *queue, int value) {
+void enqueue(struct Queue * queue, int value) {
     if (isFull(queue)) {
         printf("Queue is full. Cannot enqueue.\n");
         return;
     } else if (isEmpty(queue)) {
-        queue->front = 0;
-        queue->rear = 0;
+        queue -> front = 0;
+        queue -> rear = 0;
     } else {
-        queue->rear++;
+        queue -> rear++;
     }
-    queue->arr[queue->rear] = value;
+    queue -> arr[queue -> rear] = value;
     printf("%d enqueued to the queue.\n", value);
 }
-void dequeue(struct Queue *queue) {
+void dequeue(struct Queue * queue) {
     if (isEmpty(queue)) {
         printf("Queue is empty. Cannot dequeue.\n");
         return;
-    } else if (queue->front == queue->rear) {
-            printf("%d dequeued from the queue.\n", queue->arr[queue->front]);
+    } else if (queue -> front == queue -> rear) {
+        printf("%d dequeued from the queue.\n", queue -> arr[queue -> front]);
         initializeQueue(queue);
     } else {
-        printf("%d dequeued from the queue.\n", queue->arr[queue->front]);
-        queue->front++;
+        printf("%d dequeued from the queue.\n", queue -> arr[queue -> front]);
+        queue -> front++;
     }
 }
-void display(struct Queue *queue) {
+void display(struct Queue * queue) {
     if (isEmpty(queue)) {
         printf("Queue is empty.\n");
         return;
     }
 
     printf("Elements in the queue: ");
-    for (int i = queue->front; i <= queue->rear; i++) {
-        printf("%d ", queue->arr[i]);
+    for (int i = queue -> front; i <= queue -> rear; i++) {
+        printf("%d ", queue -> arr[i]);
     }
     printf("\n");
 }
 
 int main() {
     struct Queue queue;
-    initializeQueue(&queue);
+    initializeQueue(& queue);
 
     int choice, element;
 
@@ -65,21 +65,21 @@ int main() {
         printf("3. Display\n");
         printf("4. Quit\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
+        scanf("%d", & choice);
 
         switch (choice) {
             case 1:
                 printf("Enter the element to enqueue: ");
-                scanf("%d", &element);
-                enqueue(&queue, element);
+                scanf("%d", & element);
+                enqueue(& queue, element);
                 break;
 
             case 2:
-                dequeue(&queue);
+                dequeue(& queue);
                 break;
 
             case 3:
-                display(&queue);
+                display(& queue);
                 break;
 
             case 4:
@@ -93,4 +93,3 @@ int main() {
 
     return 0;
 }
-
